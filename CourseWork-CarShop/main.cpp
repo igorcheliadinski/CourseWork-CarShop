e43b22e665bc8c28ee5acc627788bb24f710dec1
Igor Cheliadinski <igorcheliadinski@igors-mbp.minsk.epam.com>
2014-12-09 19:53:24 +0300

    Initial Commit

---
+++ CourseWork-CarShop/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CourseWork-CarShop
+//
+//  Created by Igor Cheliadinski on 12/9/14.
+//  Copyright (c) 2014 Igor Cheliadinski. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
